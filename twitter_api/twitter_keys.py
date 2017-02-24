40434b9abec40a333485efcf244940dcdd47c58a
Tania Allard <t.sanchez@sheffield.ac.uk>
2017-02-24 15:33:04 +0000

    deleted unnecessary files

--- twitter_api/twitter_keys.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#keys 
-
-
-consumer_key = 	'qMAcxFEBQfRNxPKVtFhFZxMoS'
-consumer_secret = 'xa1RuxwukiDW7Oo0CrW59NHbN3VCANTBy1YEgkjcly3JEqXv9Q'
-access_token = '329179614-HELOxv2Tf3BdTQPsa5Xvb1Eq3GG5JA1zARvoe8Kh'
-access_token_sec = 'XguFcAJOHsT3kVIdFvT3gF4tcjW4ctsJVRUzy2u9tyjJ5'
-
