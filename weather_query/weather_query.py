0c10c984f5d7a04aa5517dfc89e21a3dcb7973d2
Tania Allard <t.allard@sheffield.ac.uk>
2017-03-09 11:33:50 +0000

    updated weather query

--- weather_query/weather_query.py
+++ weather_query/weather_query.py
@@ -1,74 +1,80 @@
 #==============================================================================
-# 
+#
 # Code used to query weather data via API
-# 
+#
 #==============================================================================
 # loading libraries
 
 import json
-import urllib.request 
 import sys
 import webbrowser
 import requests
 
-# for python2 comment the above line 
-#uncomment the one below
-# import urllib2.request
+# For python3
+import urllib.request
 
-# our variables 
+# For  python2
+from urrllib2 import urlopen
+
+# our variables
 api_id = "84600bca7507293656495e8972aec659"
-city_id= "London" # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.g
-mode =  "json"  #html, json, xml
+city_id = "Sheffield" # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.g
+mode =  "html"  #html, json, xml
 unit = 'metric'  # For Fahrenheit use imperial, for Celsius use metric, and the default is Kelvin.
 
 
 # small function to create the url
-def url_builder(city_id): 
-    api = 'http://api.openweathermap.org/data/2.5/weather?q='     
+def url_builder(city_id):
+    api = 'http://api.openweathermap.org/data/2.5/weather?q='
     full_api_url = api + str(city_id) + '&mode=' + mode + '&units=' + unit + '&APPID=' + api_id
     return full_api_url
 
 
-# using a function to fetch the weather 
+# using a function to fetch the weather
 def data_fetch(full_api_url):
+    # Python 3
     with urllib.request.urlopen(full_api_url) as url:
+
+    #Python 2
+    #with urlopen(full_api_url)as url:
       return json.loads(url.read().decode('utf-8'))
-  
-    
+
+
 # calling the above function with our vars
 url = url_builder(city_id)
 
 
 ############################################################
-# fecthing the weather 
+# fecthing the weather
 try:
     requested = requests.get(url)
-    print( requested ) # so that we can see the status
+    print("Satuts of the request:",format(requested) )
+    # so that we can see the status
     if mode == 'json':
         data = json.loads(requested.text)
     elif mode == 'html':
-        webbrowser.open_new_tab(requested.url)     
+        webbrowser.open_new_tab(requested.url)
 except:
     sys.stderr.write("Couldn't load current conditions\n")
-    
-    
+
+
 # printing the obtained data
 print( data )
 
 ###########################################################
 
-# or using the function 
+# or using the function
 data = data_fetch(url)
 print( data )
 
-# extracting the data you want 
+# extracting the data you want
 temperature = data['main']['temp']      # current temperature
-temperature_unit = 'F' if (unit == 'imperial') else 'C'    
-conditions = data['weather'][0]['description'] #current weather 
+temperature_unit = 'F' if (unit == 'imperial') else 'C'
+conditions = data['weather'][0]['description'] #current weather
 
 
 # print a simple summary
-sum_w = "{0} with a temperature of {1}" u"\u00B0" "{2}"
+sum_w = "\n \n The weather is: \n {0} with a temperature of {1}" u"\u00B0" "{2}"
 
 
 print (sum_w.format(conditions[0].upper() + conditions[1:].lower(),
